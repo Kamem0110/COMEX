0d64d6aea5361ca6ca00974c578897b3438dbcea
Kamem0110 <pedro_giusti@outlook.com>
2023-10-13 21:26:46 -0300

    Semana 5 - Pedro Giusti - Criar lista de produtos

--- src/index.php
+++ src/index.php
@@ -8,15 +8,31 @@ os quais devem ter nome e preço.
 Após, exiba-os.
 */
 
-$produtos1 = ['Produto' => 'Guaraná - 2Lt', 'Valor' => 10];
-$produtos2 = ['Produto' => 'Suco de Laranja', 'Valor' => 10,99];
-$produtos3 = ['Produto' => 'Cachorro-Quente', 'Valor' => 20];
-$produtos4 = ['Produto' => 'Pizza', 'Valor' => 30];
-$produtos5 = ['Produto' => 'Pão frânces', 'Valor' => 30];
+$produtos = [
+    [
+    'Produto' => 'Guaraná - 2Lt',
+     'Valor' => 10
+    ],
+    [
+    'Produto' => 'Suco de Laranja',
+     'Valor' => 10,99
+    ],
+    [
+    'Produto' => 'Cachorro-Quente',
+    'Valor' => 20
+    ],
+    [
+    'Produto' => 'Pizza',
+    'Valor' => 30
+    ],
+    [
+    'Produto' => 'Pão frânces',
+    'Valor' => 30
+    ]
+];
 
-$contas = [$produtos1,$produtos2,$produtos3,$produtos4,$produtos5];
-
-for ($i=0; $i < count($contas);$i++){
-    echo $contas[$i]['Produto'] . PHP_EOL;
-    echo $contas[$i]['Valor'] . PHP_EOL;
+foreach ($produtos as $produto){
+    echo "Produto " . $produto['Produto'] . 
+    ", Valor: R$" . $produto['Valor'] . PHP_EOL;
 }
+ 
