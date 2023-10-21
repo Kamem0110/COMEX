1ac5fac0beec31203aaedadc59b38a875fc1d0fe
Kamem0110 <pedro_giusti@outlook.com>
2023-10-21 18:06:34 -0300

    Semana 6 - Criar uma classe de Produto

--- src/Produto.php
+++ src/Produto.php
@@ -1,51 +1,54 @@
 <?php
 
-class Produto{
+class Produto {
     public string $nomeProduto;
-    public string $preco;
-    public string $estoque;
+    public float $preco;
+    public int $estoque;
 
-    public function __construct($nomeProduto,$preco,$estoque){
+    public function __construct(string $nomeProduto, float $preco, int $estoque) {
         $this->setNomeProduto($nomeProduto);
         $this->setPreco($preco);
-        $this->setEstoque($estoque);
-    }
-
-    public function getNomeProduto()
-    {
-        return $this->nomeProduto;
-    }
-    public function setNomeProduto(string $nomeProduto): void
-    {
-        $this->nomeProduto = $nomeProduto;
-    }
-
-    public function getPreco(): string
-    {
-        return $this->preco;
-    }
-    public function setPreco(string $preco): void
-    {
-        $this->preco = $preco;
-    }
-
-    public function getEstoque(): string
-    {
-        return $this->estoque;
-    }
-    public function setEstoque(string $estoque): void
-    {
         $this->estoque = $estoque;
     }
 
+    public function getNomeProduto(): string {
+        return $this->nomeProduto;
+    }
+    public function setNomeProduto(string $nomeProduto): void {
+        $this->nomeProduto = $nomeProduto;
+    }
+
+    public function getPreco(): float {
+        return $this->preco;
+    }
+    public function setPreco(float $preco): void {
+        $this->preco = $preco;
+    }
+
+    public function abaixarEstoque(int $diminuir): void {
+        if ($diminuir > $this->estoque) {
+            echo "Acabou no estoque";
+            return;
+        }
+        $this->estoque -= $diminuir;
+    }
+
+    public function aumentarEstoque(int $aumentar): void {
+        $this->estoque += $aumentar;
+    }
 }
 
-$frango1 = new Produto("Frango","49,89/uni", "326" );
+$frango1 = new Produto("Frango", 49.89, 326);
 
 echo "Produto: " . $frango1->getNomeProduto() . PHP_EOL;
 echo "Preço: " . $frango1->getPreco() . PHP_EOL;
-echo "Estoque: " . $frango1->getEstoque() . PHP_EOL;
+echo "Estoque: " . $frango1->estoque . PHP_EOL;
+echo PHP_EOL;
 
-//echo "Estoque: " . $frango1->getEstoque() . PHP_EOL;
+$frango1->abaixarEstoque(2);
+echo "Produto: " . $frango1->getNomeProduto() . " Preço: " . $frango1->getPreco() . " Estoque: " . $frango1->estoque . PHP_EOL;
+echo PHP_EOL;
 
+$frango1->aumentarEstoque(50);
+echo "Produto: " . $frango1->getNomeProduto() . " Preço: " . $frango1->getPreco() . " Estoque: " . $frango1->estoque . PHP_EOL;
 
